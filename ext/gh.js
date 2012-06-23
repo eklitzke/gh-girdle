4657e7fc32087a9007a46ae2951993a5eef45bd6
Evan Klitzke <evan@eklitzke.org>
2012-06-23 15:40:23 -0700

    animate things

--- ext/gh.js
+++ ext/gh.js
@@ -6,6 +6,38 @@
 // ==/UserScript==
 
 (function() {
+    var animations_ = {};
+
+    var createAnimation = (function($elem) {
+        var height = $elem._girdle_height;
+        if (animations_.hasOwnProperty(height)) {
+            return animations_[height];
+        }
+        var css = document.createElement('style');
+        css.type = 'text/css';
+        var ruleName = 'slider' + height;
+        var rules = document.createTextNode(
+            '.' + ruleName + ' {\n' +
+                '-webkit-animation-name: anim' + ruleName + ';\n' +
+                '-webkit-animation-duration: 0.5s;\n' +
+                '}\n' +
+                '\n' +
+                '.reverse-' + ruleName + ' {\n' +
+                '-webkit-animation-name: anim' + ruleName + ';\n' +
+                '-webkit-animation-duration: 0.5s;\n' +
+                '-webkit-animation-direction: reverse;\n' +
+                '}\n' +
+                '\n' +
+                '@-webkit-keyframes anim' + ruleName + ' {\n' +
+                'from { height: 0px; }\n' +
+                'to { height: ' + height + 'px; }\n' +
+                '}');
+        css.appendChild(rules);
+        $('head').append(css);
+        animations_[height] = ruleName;
+        return ruleName;
+    });
+
     var compressed = {};
     var containers = {};
 
@@ -23,37 +55,37 @@
 
                 var repo = '';
 
-                // don't handle git_hub for now.
-                if (alert_type == 'alert create') {
+                // don't handle git_hub fonow.
+                if (alert_type === 'alert create') {
                     var key = $(title_elems).get(2);
-                    if (key == undefined) {
+                    if (key === undefined) {
                         key = $(title_elems).get(0);
                     }
                     repo = $(key).text();
-                } else if (alert_type == 'alert gist') {
+                } else if (alert_type === 'alert gist') {
                     var key = $(title_elems).get(0);
                     repo = $(key).text();
-                } else if (alert_type == 'alert follow') {
+                } else if (alert_type === 'alert follow') {
                     var key = $(title_elems).get(1);
                     if ($(key).text() != user) {
                         key = $(title_elems).get(0);
                     }
                     repo = $(key).text();
-                } else if (alert_type == 'alert issues_opened' ||
-                           alert_type == 'alert issues_closed' ||
-                           alert_type == 'alert issues_reopened') {
+                } else if (alert_type === 'alert issues_opened' ||
+                           alert_type === 'alert issues_closed' ||
+                           alert_type === 'alert issues_reopened') {
                     repo = $($(title_elems).get(2)).text();
-                } else if (alert_type == 'alert push' ||
-                           alert_type == 'alert commit_comment' ||
-                           alert_type == 'alert download' ||
-                           alert_type == 'alert delete' ||
-                           alert_type == 'alert gollum' ||
-                           alert_type == 'alert fork' ||
-                           alert_type == 'alert watch_started') {
+                } else if (alert_type === 'alert push' ||
+                           alert_type === 'alert commit_comment' ||
+                           alert_type === 'alert download' ||
+                           alert_type === 'alert delete' ||
+                           alert_type === 'alert gollum' ||
+                           alert_type === 'alert fork' ||
+                           alert_type === 'alert watch_started') {
                     repo = $($(title_elems).get(1)).text();
-                } else if (alert_type == 'alert issues_comment') {
+                } else if (alert_type === 'alert issues_comment') {
                     var repo_elem = $(title_elems).get(2);
-                    if (repo_elem == undefined) {
+                    if (repo_elem === undefined) {
                         // comment on a repo
                         repo_elem = $(title_elems).get(1);
                     }
@@ -62,6 +94,9 @@
                     console.log('unknown: ' + alert_type);
                     repo = 'unknown';
                 }
+
+                // Add the data to the compressed dictionary, and
+                // remove the elements from the page.
                 if (!compressed[repo]) {
                     compressed[repo] = [];
                 }
@@ -80,6 +115,7 @@
                         containers[k] = $gh_alert;
 
                         var $body = $('<div class="body"></div>');
+                        var $body_alerts = $('<div class="girdle_hidden"></div>');
 
                         $gh_alert.append($body);
 
@@ -87,6 +123,7 @@
                         var $title = $(title);
 
                         $body.append($title);
+                        $body.append($body_alerts);
 
                         var $name = $('<a href="' + k + '">' + k + '</a>');
 
@@ -100,21 +137,48 @@
                         var $expand = $('<a id="' + k + '" class="button girdle_right">expand</a>');
 
                         $expand.click(function() {
+                            console.info('clicked');
                             var t = $expand.text();
-                            $(compressed[k]).each(function(i, value) {
-                                if (t == 'expand') {
-                                    $(value).appendTo($body);
-                                } else {
-                                    $(value).remove()
-                                }
-                            });
-                            if (t == 'expand') {
+                            var isExpand = (t === "expand");
+                            if ($body_alerts.children().length == 0) {
+                                $(compressed[k]).each(function(i, value) {
+                                    $(value).appendTo($body_alerts);
+                                });
+                            }
+                            if (isExpand) {
                                 $second_title.remove();
+                                if (!$body_alerts.hasOwnProperty('_girdle_height')) {
+                                    $body_alerts._girdle_height = $body_alerts.height();
+                                }
                                 $expand.text('compress');
                             } else {
                                 $body.append($second_title);
                                 $expand.text('expand');
                             }
+                            var ruleName = createAnimation($body_alerts);
+                            if (!isExpand) {
+                                ruleName = 'reverse-' + ruleName;
+                            }
+                            console.info('ruleName is ' + ruleName);
+
+                            //$body_alerts[0].className =
+                            //$body_alerts[0].className + " " +
+                            //ruleName;
+                            $body_alerts.addClass(ruleName);
+                            console.log($body_alerts);
+                            $body_alerts.one('webkitAnimationStart', function() {
+                                console.info('start animation');
+                            });
+                            $body_alerts.one('webkitAnimationEnd', function() {
+                                $body_alerts.removeClass(ruleName)
+                                console.info('end animation');
+                                if (isExpand) {
+                                    $body_alerts.height($body_alerts._girdle_height);
+                                } else {
+                                    $body_alerts.height(0);
+                                }
+                            });
+//                            $body.style.webKitAnimationName = 'slidedown';
                         });
 
                         $title.append($expand);
